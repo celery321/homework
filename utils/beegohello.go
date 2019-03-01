ca74b4dd0f658a13b6fcf8827a49d73dbbce655c
阿狗 <szin2012@sina.com>
2019-03-01 14:19:54 +0800

    gin hello 3

--- utils/beegohello.go
+++ utils/beegohello.go
@@ -2,6 +2,15 @@ package utils
 
 import "github.com/astaxie/beego"
 
+type HelloController struct {
+	beego.Controller
+}
+
+func (this *HelloController) Get() {
+	this.Ctx.WriteString("hello world")
+}
+
 func Beegohello() {
+	beego.Router("/", &HelloController{})
 	beego.Run()
 }
