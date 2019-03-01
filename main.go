782ced4349fa80a6d193022232596d40493930ed
阿狗 <szin2012@sina.com>
2019-03-01 10:11:04 +0800

    gin hello

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/szin2012/homework"
+
+func main() {
+
+}
--- utils/hello.go
+++ utils/hello.go
@@ -1,7 +1,7 @@
-package main
+package utils
 
 import "fmt"
 
-func main() {
+func Hello() {
 	fmt.Println("hello")
 }
